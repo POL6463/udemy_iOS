d4ca439bb185838415622e6d3a0353ec1ebfb3d9
POL6463 <kevinkim0617kim@gmail.com>
2022-06-25 18:04:17 -0700

    initial commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Minwoong Kim on 2022/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
